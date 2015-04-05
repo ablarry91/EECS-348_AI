82c177bbaafac22c6eb135ee9144504fb034a7da
Austin Lawrence <Ablarry91@gmail.com>
2015-04-05 01:42:47 -0500

    Tic-tac-toe class seems to be working fine.

--- assignment1/abl970-pa1.py
+++ assignment1/abl970-pa1.py
@@ -36,14 +36,10 @@ def median(L):
 		index = (length-1)/2+1
 		return L[index]
 
-# You will probably want to read about how lists can be used as Stacks and Queues in the Python documentation (or it may be intuitive to you to just create Stack and Queue classes). IMPORTANT: to demonstrate that your functions actually perform breadth first, and depth first search, you must print each element in the tree just before it has been examined (on a line by itself). See an example of what this looks like as output in the file pa1tests-output.txt.
-
-#breadth first: expand 4, then expand 10,3,14,1, then 33,2, then 12
 # [4, [10, [33], [2]], [3], [14, [12]], [1]]
 def bfs(tree,elem):
 	"""performs breadth-first search for a given tree and search term 'element'"""
-	from collections import deque
-	# myTree = deque(tree)
+	from collections import deque	# library that allows us to pop and append at the beginning of a list easily
 
 	# Add the initial state (root) to the <fringe>
 	fringe = deque()
@@ -56,60 +52,100 @@ def bfs(tree,elem):
 	# Choose a node (curr) to examine from the <fringe> (if there is nothing in <fringe> - FAILURE)
 	while curr != elem:
 		node = fringe[0]
-		# print 'node = ', node
 		curr = node[0]
 		for i in range(len(node)):
 			if i != 0:
 				fringe.append(node[i])
 		fringe.popleft()
 		print curr
-		# print fringe
-		# print ''
 		if len(fringe) == 0:
 			print "no match found"
-			break
-
-print "This should work:"
-bfs([4, [10, [33], [2]], [3], [14, [12]], [1]],1)
-print ''
-print "I want this to work:"
-bfs([4, [10, [33], [2]], [3], [14, [12]], [1]],2)
-print ''
-print "This should fail:"
-bfs([4, [10, [33], [2]], [3], [14, [12]], [1]],40)
-
-
-
-	# Is curr a goal state?
-	
-
-	# Expand curr by applying all possible actions (add the new resulting states to the <fringe>)
-
-
+			return False
+	return True
 
 def dfs(tree,elem):
 	"""performs depth-first search for a given tree and search term 'element'"""
-	pass
+	from collections import deque
+
+	# Add the initial state (root) to the fringe
+	myList = deque(tree)
+	node = myList[0]
+	myList.popleft()
+	curr = node
+
+	# Choose a node to examine from the fringe, repeat as necessary
+	while curr != elem:
+		node = deque(myList[0])
+		curr = node.popleft()
+		print curr
+		myList.appendleft(node)
+		print myList
+		print ''
+		if len(myList) == 0:
+			print "failure"
+			return False
+	return True
+
+print "This should work:"
+# dfs([4, [10, [33], [2]], [3], [14, [12]], [1]],1)
 
 class TTTBoard:
 	def __init__(self):
 		"""Initialize a 3x3 tic tac toe board. The board should contain a single attribute, a list, that initially contains nine star characters.  This list contains contents of the board.  A star denotes that this position has not yet been claimed by 'X' or 'O'.  Again, this is simply a flat list of 9 items, not a list of lists."""
-		pass
+		self.board = ['*','*','*','*','*','*','*','*','*']
+		self.count = 0
+
 	def __str__(self):
 		"""Returns a string representation of the board. Instead of just displaying a flat list of the 9 items, I want you to display this list like a tic tac toe board, with 3 rows of 3 items each. See the test file output for what this looks like."""
-		pass
+		return str(self.board[0])+"|"+str(self.board[1])+"|"+str(self.board[2])+"\r\n"+"--"+"--"+"--"+"\r\n"+str(self.board[3])+"|"+str(self.board[4])+"|"+str(self.board[5])+"\r\n"+"--"+"--"+"--"+"\r\n"+str(self.board[6])+"|"+str(self.board[7])+"|"+str(self.board[8])
+	
 	def makeMove(self,player,pos):
 		"""Places a move for player in the position pos (where the board squares a numbered from left to right, starting in the top left squares with 0, and beginning at the left in each new row), if possible.  'player' is a character ('X' or 'O') and pos is an integer.  Returns True if the move was made and False if not (because te spot was full, or outside the boundaries of the board)."""
-		pass
+		if self.board[pos] == "*":
+			self.board[pos] = player
+			self.count += 1
+			return True
+		else:
+			return False
+	
 	def hasWon(self,player):
 		"""Returns True if player has won the game, and False if not"""
-		pass
+
+		# check diagonals
+		if self.board[0] == player and self.board[4] == player and self.board[8] == player:
+			return True
+		elif self.board[2] == player and self.board[4] == player and self.board[6] == player:
+			return True
+
+		# check horizontals
+		elif self.board[0] == player and self.board[1] == player and self.board[2] == player:
+			return True
+		elif self.board[3] == player and self.board[4] == player and self.board[5] == player:
+			return True
+		elif self.board[6] == player and self.board[7] == player and self.board[8] == player:
+			return True
+
+		# check verticals
+		elif self.board[0] == player and self.board[3] == player and self.board[6] == player:
+			return True
+		elif self.board[1] == player and self.board[4] == player and self.board[7] == player:
+			return True
+		elif self.board[2] == player and self.board[5] == player and self.board[8] == player:
+			return True
+
+		# if nothing, return false
+		return False
+
 	def gameOver(self):
 		"""Returns True if someone has won or if the board is full, False otherwise"""
-		pass
+		if hasWon('X') or hasWon('O') or self.count>=8:
+			return True
+		else:
+			return False
+
 	def clear(self):
 		"""Clears the board to reset the game"""
-		pass
+		self.board = ['*','*','*','*','*','*','*','*','*']
 
 
 
